233402d7609b37f377f6fb2f8e4daa0e9f40b983
chuck lu <luchukun1993@gmainl.com>
2017-01-31 11:11:47 +0800

    init the rep
    Signed-off-by: chuck lu <luchukun1993@gmainl.com>

---
+++ src/main/java/sjtu/org/ReadelistApplication.java
@@ -0,0 +1,12 @@
+package sjtu.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadelistApplication.class, args);
+	}
+}
